bfec6de807cb01f3141c57de8a44ed1d46993516
Akash Rastogi <ios@quippp.com>
2014-02-11 16:40:30 +0530

    Declare celsiusToFarenhite conversion method (without definition)

--- TDD-2/ViewController.h
+++ TDD-2/ViewController.h
@@ -9,4 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+-(int)celsiusToFarenhite :(int)celsius;
 @end
--- TDD-2Tests/TDD_2Tests.m
+++ TDD-2Tests/TDD_2Tests.m
@@ -24,6 +24,7 @@ ViewController *viewController;
 - (void)tearDown
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    viewController = nil;
     [super tearDown];
 }
 
