8053892c59858005fce15b6b4a33d6d2bcd07896
Akash Rastogi <ios@quippp.com>
2014-02-13 16:07:53 +0530

    Add a validation for nil. All test passed

--- TDD-3/ViewController.m
+++ TDD-3/ViewController.m
@@ -28,37 +28,42 @@
 }
 
 -(id)romanToArabic :(NSString *)input{
-    NSDictionary *dictSymbols = @{
-                                  @"M": [NSNumber numberWithInt:1000],
-                                  @"D": [NSNumber numberWithInt:500],
-                                  @"C": [NSNumber numberWithInt:100],
-                                  @"L": [NSNumber numberWithInt:50],
-                                  @"X": [NSNumber numberWithInt:10],
-                                  @"V": [NSNumber numberWithInt:5],
-                                  @"I": [NSNumber numberWithInt:1]
-                                  };
-    
-    int totalValue = 0; // hold the total value of the roman numeral
-    int previousValue = 0; // Set the value of the previous pointer to some MIN value.
-    int currentValue =0; // This will hold the value of the current Roman Numeral character.
-    
-    for (int i=[input length]; i>=1; i--) {
-        NSString *current = [[input uppercaseString] substringWithRange:NSMakeRange(i-1, 1)];
-        if (![[dictSymbols allKeys]containsObject:current]) {/// invalid character found, set the value to -1 and break the loop.
-            totalValue = -1;
-            break;
+    NSNumber *output;
+    if (input) {
+        NSDictionary *dictSymbols = @{
+                                      @"M": [NSNumber numberWithInt:1000],
+                                      @"D": [NSNumber numberWithInt:500],
+                                      @"C": [NSNumber numberWithInt:100],
+                                      @"L": [NSNumber numberWithInt:50],
+                                      @"X": [NSNumber numberWithInt:10],
+                                      @"V": [NSNumber numberWithInt:5],
+                                      @"I": [NSNumber numberWithInt:1]
+                                      };
+        
+        int totalValue = 0; // hold the total value of the roman numeral
+        int previousValue = 0; // Set the value of the previous pointer to some MIN value.
+        int currentValue =0; // This will hold the value of the current Roman Numeral character.
+        
+        for (int i=[input length]; i>=1; i--) {
+            NSString *current = [[input uppercaseString] substringWithRange:NSMakeRange(i-1, 1)];
+            if (![[dictSymbols allKeys]containsObject:current]) {/// invalid character found, set the value to -1 and break the loop.
+                totalValue = -1;
+                break;
+            }
+            
+            currentValue = [[dictSymbols valueForKey:current]integerValue];
+            
+            if (currentValue < previousValue)
+                totalValue = totalValue - currentValue;
+            else
+                totalValue = totalValue + currentValue;
+            
+            previousValue = currentValue  ;
         }
-        
-        currentValue = [[dictSymbols valueForKey:current]integerValue];
-        
-        if (currentValue < previousValue)
-            totalValue = totalValue - currentValue;
-        else
-            totalValue = totalValue + currentValue;
-        
-        previousValue = currentValue  ;
+        output = [NSNumber numberWithInteger:totalValue];
     }
-    NSNumber *output = [NSNumber numberWithInteger:totalValue];
+    else output = [NSNumber numberWithInteger:-1];
+    
     return output;
 }
 
