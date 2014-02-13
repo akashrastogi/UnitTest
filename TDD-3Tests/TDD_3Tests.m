90df3e10092933e5afeab7d8762472ea24db10ce
Akash Rastogi <ios@quippp.com>
2014-02-13 11:16:04 +0530

    First Test case for TWO - Failed

--- TDD-3Tests/TDD_3Tests.m
+++ TDD-3Tests/TDD_3Tests.m
@@ -42,4 +42,18 @@ ViewController *viewController;
     XCTAssertEqualObjects(desiredRoman, actualRoman, @"Integer to Roman test is failed");
 }
 
+- (void)testFor_Two
+{
+    NSString *romanNumeral = @"II";
+    NSNumber *desiredNumber = [NSNumber numberWithInt:2];
+    NSNumber *actualNumber = [viewController romanToIntegerAndIntegertoRoman:romanNumeral];
+    XCTAssertEqual(desiredNumber, actualNumber, @"Roman to integer test is failed.");
+    
+    
+    NSNumber *number = [NSNumber numberWithInt:2];
+    NSString *desiredRoman = @"II";
+    NSString *actualRoman = [viewController romanToIntegerAndIntegertoRoman:number];
+    XCTAssertEqualObjects(desiredRoman, actualRoman, @"Integer to Roman test is failed");
+}
+
 @end
