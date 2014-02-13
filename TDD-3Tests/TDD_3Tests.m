96258dee2240f793fa47bb8fcefcdf3bde3151d6
Akash Rastogi <ios@quippp.com>
2014-02-13 16:04:18 +0530

    Write test case for nil, test failed

--- TDD-3Tests/TDD_3Tests.m
+++ TDD-3Tests/TDD_3Tests.m
@@ -131,4 +131,12 @@ ViewController *viewController;
     NSNumber *actualNumber = [viewController romanToArabic:romanNumeral];
     XCTAssertEqualObjects(desiredNumber, actualNumber, @"Roman to integer test is failed.");
 }
+
+- (void)testFor_Nil
+{
+    NSString *romanNumeral = nil;
+    NSNumber *desiredNumber = [NSNumber numberWithInt:-1];
+    NSNumber *actualNumber = [viewController romanToArabic:romanNumeral];
+    XCTAssertEqualObjects(desiredNumber, actualNumber, @"Roman to integer test is failed.");
+}
 @end
